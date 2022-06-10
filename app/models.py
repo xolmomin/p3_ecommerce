37e4a9a9eb2779f31f62468c03ae0338a9d608e2
Rakhmatov0107 <uktamrakhmatov4003@gmail.com>
2022-06-11 00:02:22 +0600

    remove lines

--- app/models.py
+++ app/models.py
@@ -10,14 +10,11 @@ from django.utils.text import slugify
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-
         if not email:
             raise ValueError('Users must have a phone number!')
-
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
