58f8cd0d81bd37be31251bc75550c9c43f5cf860
Rakhmatov0107 <uktamrakhmatov4003@gmail.com>
2022-06-10 23:56:51 +0600

    add second new features

--- app/models.py
+++ app/models.py
@@ -17,6 +17,7 @@ class UserManager(BaseUserManager):
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
